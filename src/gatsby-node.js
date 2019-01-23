ed043185944cf8e46b78ae7a75d0250b09e3136b
Giorgio Aquino <giorgio@uberresearch.com>
2019-01-23 15:17:30 +0100

    remove co depedencies

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,5 +1,4 @@
 const Twitter = require("twitter");
-const co = require("co");
 const fs = require("fs");
 const querystring = require("querystring");
 
@@ -8,92 +7,93 @@ const { md5 } = require("./utils");
 const { twitterType } = require("./schema");
 
 function generateNode(tweet) {
-  const contentDigest = md5(JSON.stringify(tweet));
-  const id = md5(tweet.id_str);
+    const contentDigest = md5(JSON.stringify(tweet));
+    const id = md5(tweet.id_str);
 
-  const nodeData = {
-    id: id,
-    children: [],
-    parent: `__SOURCE__`,
-    internal: {
-      type: `Tweet`,
-      contentDigest
+    const nodeData = {
+        id: id,
+        children: [],
+        parent: `__SOURCE__`,
+        internal: {
+            type: `Tweet`,
+            contentDigest
+        }
+    };
+
+    // Removing place.bounding_box duo coordinates GraphQL issue
+    // Help wantend!
+    if (tweet && tweet.place && tweet.place.bounding_box) {
+        delete tweet.place.bounding_box;
+        tweet.place.bounding_box = null;
     }
-  };
 
-  // Removing place.bounding_box duo coordinates GraphQL issue
-  // Help wantend!
-  if (tweet && tweet.place && tweet.place.bounding_box) {
-    delete tweet.place.bounding_box;
-    tweet.place.bounding_box = null;
-  }
+    const node = Object.assign({}, tweet, nodeData);
 
-  const node = Object.assign({}, tweet, nodeData);
-
-  return node;
+    return node;
 }
 
-exports.sourceNodes = (
-  { boundActionCreators },
-  {
-    q,
-    credentials,
-    count = 100,
-    tweet_mode = "compat",
-    result_type = "mixed",
-    fetchAllResults = false,
-    debug = false
-  }
+exports.sourceNodes = async (
+    { boundActionCreators },
+    {
+        q,
+        credentials,
+        count = 100,
+        tweet_mode = "compat",
+        result_type = "mixed",
+        fetchAllResults = false,
+        debug = false
+    }
 ) => {
-  const { createNode } = boundActionCreators;
+    const { createNode } = boundActionCreators;
 
-  function createNodes(tweets) {
-    tweets.forEach(tweet => {
-      createNode(generateNode(tweet));
+    function createNodes(tweets) {
+        tweets.forEach(tweet => {
+            createNode(generateNode(tweet));
+        });
+    }
+
+    var client = new Twitter(credentials);
+
+    let results = await client.get("search/tweets", {
+        q,
+        count,
+        include_entities: true,
+        tweet_mode,
+        result_type
     });
-  }
-
-  var client = new Twitter(credentials);
-
-  return co(function*() {
-    // Get Twitter
-    let results = yield client.get("search/tweets", {
-      q,
-      count,
-      include_entities: true,
-      tweet_mode,
-      result_type
-    });
-
+    // saveResult(results);
     createNodes(results.statuses);
 
     if (fetchAllResults === true) {
-      while (results.search_metadata.next_results) {
-        let params = querystring.parse(
-          results.search_metadata.next_results.substr(1)
-        );
+        while (results.search_metadata.next_results) {
+            let params = querystring.parse(
+                results.search_metadata.next_results.substr(1)
+            );
 
-        params.tweet_mode = tweet_mode;
-        params.result_type = result_type;
+            params.tweet_mode = tweet_mode;
+            params.result_type = result_type;
 
-        results = yield client.get("search/tweets", params);
+            results = await client.get("search/tweets", params);
 
-        createNodes(results.statuses);
-      }
+            createNodes(results.statuses);
+        }
     }
-  });
 };
 
 function saveResult(results, index = 0) {
-  // TODO: ADD DEBUG MODE
-      fs.writeFileSync(`./tweets-${index}.json`, JSON.stringify(results, null, 4), {
-        encoding: "utf8"
-      });
+    // TODO: ADD DEBUG MODE
+    fs.writeFileSync(
+        `./tweets-${index}.json`,
+        JSON.stringify(results, null, 4),
+        {
+            encoding: "utf8"
+        }
+    );
 }
 
 exports.setFieldsOnGraphQLNodeType = ({ type }) => {
-  if (type.name !== `Tweet`) {
-    return {};
-  }
-  return twitterType;
+    if (type.name !== `Tweet`) {
+        return {};
+    }
+    return twitterType;
 };
