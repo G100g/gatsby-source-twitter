54cd91cf425570080ea396191c7ca023c4cd6267
Giorgio Aquino <giorgio@uberresearch.com>
2019-01-26 14:44:16 +0100

    improve logice and add debug mode

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -6,6 +6,9 @@ const { md5 } = require("./utils");
 
 const { twitterType } = require("./schema");
 
+const logNamespace = `\x1b[36mtwitter\x1b[0m`;
+const debugNamespace = `\x1b[33mtwitter\x1b[0m`;
+
 function generateNode(tweet) {
     const contentDigest = md5(JSON.stringify(tweet));
     const id = md5(tweet.id_str);
@@ -52,36 +55,65 @@ exports.sourceNodes = async (
         });
     }
 
-    var client = new Twitter(credentials);
+    function log(msg, namespace = logNamespace) {
+        console.log(`\n${namespace} ${msg}`);
+    }
 
-    let results = await client.get("search/tweets", {
-        q,
-        count,
-        include_entities: true,
-        tweet_mode,
-        result_type
-    });
-    // saveResult(results);
-    createNodes(results.statuses);
-
-    if (fetchAllResults === true) {
-        while (results.search_metadata.next_results) {
-            let params = querystring.parse(
-                results.search_metadata.next_results.substr(1)
-            );
-
-            params.tweet_mode = tweet_mode;
-            params.result_type = result_type;
-
-            results = await client.get("search/tweets", params);
-
-            createNodes(results.statuses);
+    function logDebug(msg) {
+        if (debug === true) {
+            log(msg, debugNamespace);
         }
     }
+
+    var client = new Twitter(credentials);
+
+    const results = [];
+
+    log("Fetching Twitter content...");
+    results.push(
+        await client.get("search/tweets", {
+            q,
+            count,
+            include_entities: true,
+            tweet_mode,
+            result_type
+        })
+    );
+
+    if (fetchAllResults === true) {
+        let [lastResult] = results;
+        if (
+            lastResult &&
+            lastResult.search_metadata &&
+            lastResult.search_metadata.next_results
+        ) {
+            log("Fetching all Twitter results...");
+            while (lastResult.search_metadata.next_results) {
+                let params = querystring.parse(
+                    lastResult.search_metadata.next_results.substr(1)
+                );
+                params.tweet_mode = tweet_mode;
+                params.result_type = result_type;
+
+                logDebug(JSON.stringify(params, null, 2));
+
+                lastResult = await client.get("search/tweets", params);
+
+                results.push(lastResult);
+            }
+        }
+    }
+
+    log("Creating Twitter nodes...");
+    results.forEach((result, index) => {
+        createNodes(result.statuses);
+        if (debug === true) {
+            saveResult(results, index);
+        }
+    });
 };
 
 function saveResult(results, index = 0) {
-    // TODO: ADD DEBUG MODE
     fs.writeFileSync(
         `./tweets-${index}.json`,
         JSON.stringify(results, null, 4),
